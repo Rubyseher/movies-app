ec5d060514c2e78c96ec8ec02b1313786e84fb4a
Rubyseher <rubyseher2002@gmail..com>
2025-07-10 09:54:41 +0530

    search bar

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -3,10 +3,16 @@ import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
 function App() {
 
+  const handelSubmit=()=>{
+
+  }
   return (
     <div>
     <MovieCard url={'something'} title="movie name"/>
     <Home/>
+    <form onSubmit={()=> handelSubmit()} >
+      <input type="text" ></input>
+    </form>
     </div>
   )
 }
